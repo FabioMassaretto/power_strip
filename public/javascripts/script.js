0c40dc8e73a0ccd61622e14e75c6bdd4bb8dfb53
krpeacock <kylpeacock@gmail.com>
2016-04-06 13:34:09 -0700

    changes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,34 +5,39 @@ var data = {lamp:'off', heater:'off'};
 $('#flip-checkbox-1').on('change', function(e){
   if (e.target.checked){
     data.lamp = 'on';
-    post('lamp');
-
   } else {
     data.lamp = 'off';
-    post('lamp');
   }
-}
-)
-
-$('#flip-checkbox-2').on('change', function(e){
-  if (e.target.checked){
-    data.heater = 'on';
-    post('heater');
-  } else {
-    data.heater = 'off';
-    post('heater');
-  }
-})
-
-function post(switch){
   $.ajax({
     type: 'POST',
     data: JSON.stringify(data),
     contentType: 'application/json',
-    url: `http://10.0.1.23:3000/${switch}`,
+    url: 'http://10.0.1.23:3000/lamp',
     success: function(data) {
     console.log('success');
     console.log(JSON.stringify(data));
   }
   })
+})
+
+$('#flip-checkbox-2').on('change', function(e){
+  if (e.target.checked){
+    data.heater = 'on';
+  } else {
+    data.heater = 'off';
+  }
+  $.ajax({
+    type: 'POST',
+    data: JSON.stringify(data),
+    contentType: 'application/json',
+    url: 'http://10.0.1.23:3000/heater',
+    success: function(data) {
+    console.log('success');
+    console.log(JSON.stringify(data));
+  }
+  })
+})
+
+function post(switch){
+
 }

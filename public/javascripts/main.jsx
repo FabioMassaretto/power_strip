069cf8c772a0982cf8c2efc0a6192d3755dea573
krpeacock <kylpeacock@gmail.com>
2016-06-16 17:29:57 -0700

    working react toggle functionality

--- public/javascripts/main.jsx
+++ public/javascripts/main.jsx
@@ -1,11 +1,56 @@
-var StripClient = React.createClass({displayName: 'StripClient',
+var StripClient = React.createClass({
+  displayName: 'StripClient',
+
+  getInitialState : function getInitialState() {
+      return {
+          switches : []  
+      };
+  },
+
+  componentWillMount: function componentWillMount() {
+    $.getJSON("/api/switches").then((function (switches) {
+      this.setState({
+        switches: switches
+      });
+    }).bind(this));
+  },
+
+//ID is formatted as "sw1"
+  toggleSwitch: function toggleSwitch(id) {
+    var route = "/api/switches/" + id;
+    $.post(route).then((data)=>{
+      this.state.switches[id[2] - 1] = data;
+      this.setState({
+           switches: this.state.switches
+        });
+        console.log(this.state.switches)
+      }.bind(this)
+    )
+  },
+
   render: function(){
+    if (this.state.switches){
+      var switchStates = this.state.switches.map((v,i)=>{
+        return (
+          <div className="switch" key={i}>
+            <Switch
+              id={v.id}
+              number={i + 1}
+              state={v.state}
+              toggleSwitch={this.toggleSwitch}
+            />
+          </div>
+        )
+      })
+    }
     return (
-      <div>
+      <div className="container">
         <h1>Power Strip</h1>
-        <Switch></Switch>
+        {switchStates}
       </div>
     )
+    
+
   }
 });
 
@@ -13,7 +58,13 @@ var Switch = React.createClass({displayName: 'Switch',
   render: function(){
     return (
       <div>
-        <button>Ima button</button>
+        <h4>Switch #{this.props.number}</h4>
+          <p>{this.props.state === "on" ? "Switch is ON" : "Switch is OFF"}</p>
+          <button 
+            type="button"
+            onClick={()=>{this.props.toggleSwitch(this.props.id)}} 
+          >Toggle
+          </button>
       </div>
     )
   }

280cd428b5894d8a40c35b454b1dccbbc976a631
krpeacock <kylpeacock@gmail.com>
2016-06-30 15:48:39 -0700

    some animations for the navbar working

---
+++ public/views/main.jsx
@@ -0,0 +1,110 @@
+var StripClient = React.createClass({
+  displayName: 'StripClient',
+
+  getInitialState : function getInitialState() {
+      return {
+          switches : []  
+      };
+  },
+
+  componentWillMount: function componentWillMount() {
+    $.getJSON("/api/switches").then((function (switches) {
+      this.setState({
+        switches: switches
+      });
+    }).bind(this));
+  },
+
+//ID is formatted as "sw1"
+  toggleSwitch: function toggleSwitch(id) {
+    var route = "/api/switches/" + id;
+    $.post(route).then((data)=>{
+      this.state.switches[id[2] - 1] = data;
+      this.setState({
+           switches: this.state.switches
+        });
+        console.log(this.state.switches)
+      }.bind(this)
+    )
+  },
+
+  changeName: function changeName(id, name){
+    var route = "/api/switches/" + id + "/name";
+    $.post(route, name).then((data)=>{
+      this.state.switches[id[2] - 1] = data;
+      this.setState({
+        switches: this.state.switches
+      });
+      console.log(this.state.switches)
+    }.bind(this))
+  },
+
+  render: function(){
+    if (this.state.switches){
+      var switchStates = this.state.switches.map((v,i)=>{
+        return (
+          <div className="switch" key={i}>
+            <Switch
+              id={v.id}
+              name={v.name}
+              number={i + 1}
+              state={v.state}
+              showInput={false}
+              toggleSwitch={this.toggleSwitch}
+              changeName={this.changeName}
+              nameInput={function(){this.showInput = true; }.bind(this)}
+            />
+          </div>
+        )
+      })
+    }
+    return (
+      <div className="container">
+        <h1>Power Strip</h1>
+        {switchStates}
+      </div>
+    )
+    
+
+  }
+});
+
+var Switch = React.createClass({displayName: 'Switch',
+  render: function(){
+      if (this.props.showInput){
+        return (
+          <div>
+            <input type="text"/>
+              <a
+                href="#" 
+                id={this.props.id}
+                className={"toggle " + (this.props.state === "on" ? "toggle--on" : "toggle--off")}
+                onClick={()=>{this.props.toggleSwitch(this.props.id)}} 
+              >
+              </a>
+          </div>
+        )
+      }
+      else {
+        return (
+        <div>
+          <h4
+            onClick={()=>{this.props.nameInput()}}
+          >{this.props.name}</h4>
+            <a
+              href="#" 
+              id={this.props.id}
+              className={"toggle " + (this.props.state === "on" ? "toggle--on" : "toggle--off")}
+              onClick={()=>{this.props.toggleSwitch(this.props.id)}} 
+            >
+            </a>
+        </div>
+        )
+      }
+  }
+});
+
+ReactDOM.render(
+  React.createElement(StripClient, null),
+  document.getElementById('strip')
+);
---
+++ public/views/navbar.jsx
@@ -0,0 +1,90 @@
+var Navbar = React.createClass ({
+    displayName: "Navbar",
+
+    getInitialState : function getInitialState() {
+        return {
+          isMenuOpen: false,
+        };
+    },
+
+    openIconMenu: function openIconMenu(){
+      $('.gn-menu-wrapper').addClass('gn-open-part')
+    },
+
+    closeIconMenu: function closeIconMenu(){
+      $('.gn-menu-wrapper').removeClass('gn-open-part')
+    },
+
+    openMenu: function openMenu(){
+      // trigger
+      $('.gn-icon-menu').addClass('gn-selected')
+      
+      // menu
+      $('.gn-menu-wrapper').addClass('gn-open-all')
+      this.setState({isMenuOpen: true})
+      this.closeIconMenu();
+    },
+
+    closeMenu: function(){
+      if (this.isMenuOpen) return;
+
+      $('.gn-icon-menu').removeClass('gn-selected')
+      this.setState({isMenuOpen: false})
+      $('.gn-menu-wrapper').removeClass('gn-open-all')
+
+      this.closeIconMenu();
+
+    },
+
+    menuToggle: function menuToggle(){
+      event.stopPropagation();
+      event.preventDefault();
+      if (this.state.isMenuOpen){
+        this.closeMenu();
+      }
+      else {
+        this.openMenu();
+      }
+    },
+
+    render: function(){
+      return (
+        <div className="navbar" fluid>
+          <ul id="gn-menu" className="gn-menu-main">
+            <li className="gn-trigger">
+              <a 
+                className="gn-icon gn-icon-menu"
+                onMouseEnter={this.openIconMenu}
+                onMouseLeave={this.closeIconMenu}
+                onClick={this.menuToggle}
+              >
+                <span>Menu</span>
+              </a>
+              <nav 
+                className="gn-menu-wrapper"
+                onMouseOver={this.openMenu}
+              >       
+                <div className="gn-scroller">
+                  <ul className="gn-menu">
+                    <li><a className="gn-icon gn-icon-cog">Settings</a></li>
+                    <li><a href="#">Switch 1</a></li>
+                    <li><a href="#">Switch 2</a></li>
+                    <li><a href="#">Switch 3</a></li>
+                    <li><a href="#">Switch 4</a></li>
+                    <li><a href="#">Switch 5</a></li>
+                  </ul>
+                </div> 
+              </nav>
+            </li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+          </ul>
+        </div>
+      );
+    }
+})
+
+ReactDOM.render(
+  React.createElement(Navbar, null),
+  document.getElementById('navbar')
+);

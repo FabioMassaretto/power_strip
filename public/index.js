58e020e8571bbc613eabfce74db7e66aa802e9e2
krpeacock <kylpeacock@gmail.com>
2016-07-01 09:41:07 -0700

    React router initialized

--- public/index.js
+++ public/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/app.jsx'
 
-render(<App/>, document.getElementById('app'))
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+  ), document.getElementById('app'))
